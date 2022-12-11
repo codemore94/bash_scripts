89fe56e4e98911e901978b17bb8cc25a6ab5f481
Mikko Saari <mikko.l.saari@tuni.fi>
2022-12-11 10:40:00 +0200

    new_sub_directory

---
+++ BASH_SCRIPTS/news_bash.sh
@@ -0,0 +1,1 @@
+ls -lt | head -n 4 >> news | cut -f 10 news | cut -c 47-60
---
+++ BASH_SCRIPTS/undo_mod.sh
@@ -0,0 +1,4 @@
+
+cp -r /home/mikko/SW_TEST22/TestausProju/COMP.SE.200-2022-2023-1/src/test_add.js test_add_cache.js
+cp -r /home/mikko/SW_TEST22/TestausProju/COMP.SE.200-2022-2023-1/src/help_test.js help_test_cache.js
+cp -r /home/mikko/SW_TEST22/TestausProju/COMP.SE.200-2022-2023-1/src/test_add.js~ test_add_cache.js
